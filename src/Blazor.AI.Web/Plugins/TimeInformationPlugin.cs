4c50489fdc8134375d9cdf57cd509a481c55bebd
glensouza <glenster75@gmail.com>
2025-01-14 14:44:00 -0800

    Added time and search plugins

---
+++ src/Blazor.AI.Web/Plugins/TimeInformationPlugin.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.SemanticKernel;
+using System.ComponentModel;
+
+namespace Blazor.AI.Web.Plugins;
+
+public class TimeInformationPlugin
+{
+    [KernelFunction]
+    [Description("Retrieves the current time in UTC.")]
+    public string GetCurrentUtcTime() => DateTime.UtcNow.ToString("R");
+}
