8cfbe4d9dc604e13c4797e5063a21d1e9579c047
glensouza <glenster75@gmail.com>
2025-01-18 01:23:56 -0800

    Add timing reports for configuration, client initialization, index creation, and document upload processes

--- src/Blazor.AI.SeedData/Program.cs
+++ src/Blazor.AI.SeedData/Program.cs
@@ -27,9 +27,12 @@ IProgress<string> progress = new Progress<string>(Console.WriteLine);
 
 // Build configuration from user secrets
 progress.Report("Getting values from configuration...");
+Stopwatch stopwatch = Stopwatch.StartNew();
 IConfigurationRoot config = new ConfigurationBuilder()
     .AddUserSecrets(Assembly.GetExecutingAssembly(), true)
     .Build();
+stopwatch.Stop();
+progress.Report($"End Getting values from configuration... (Elapsed time: {stopwatch.ElapsedMilliseconds} ms)");
 
 // Retrieve configuration values from user secrets
 string aoaiKey = config["AzureOpenAI:Key"] ?? throw new Exception("AzureOpenAI:Key needs to be set");
@@ -44,19 +47,23 @@ progress.Report("End Getting values from configuration...");
 
 // Initialize Azure OpenAI and Search clients
 progress.Report("Initializing Azure OpenAI and Search clients...");
+stopwatch.Restart();
 AzureOpenAIClient azureClient = new(new Uri(aoaiEndpoint), new ApiKeyCredential(aoaiKey));
 ChatClient chatClient = azureClient.GetChatClient(aoaiChatDeploymentName);
 EmbeddingClient embedClient = azureClient.GetEmbeddingClient(aoaiEmbeddingModel);
 SearchIndexClient indexClient = new(new Uri(searchServiceEndpoint), new AzureKeyCredential(searchApiKey));
-progress.Report("End Initializing Azure OpenAI and Search clients...");
+stopwatch.Stop();
+progress.Report($"End Initializing Azure OpenAI and Search clients... (Elapsed time: {stopwatch.ElapsedMilliseconds} ms)");
 
 // Delete existing index if it exists
 try
 {
     progress.Report("Deleting existing index...");
+    stopwatch.Restart();
     indexClient.GetIndex(indexName);
     indexClient.DeleteIndex(indexName);
-    progress.Report("Index deleted successfully.");
+    stopwatch.Stop();
+    progress.Report($"Index deleted successfully. (Elapsed time: {stopwatch.ElapsedMilliseconds} ms)");
 }
 catch (RequestFailedException ex) when (ex.Status == 404)
 {
@@ -77,8 +84,7 @@ SearchIndex searchIndex = new(indexName)
     {
         Profiles =
         {
-            new VectorSearchProfile(vectorSearchHnswProfile, vectorSearchHnswConfig)
-                { VectorizerName = vectorSearchVectorizer }
+            new VectorSearchProfile(vectorSearchHnswProfile, vectorSearchHnswConfig) { VectorizerName = vectorSearchVectorizer }
         },
         Algorithms = { new HnswAlgorithmConfiguration(vectorSearchHnswConfig) },
         Vectorizers =
@@ -101,17 +107,19 @@ SearchIndex searchIndex = new(indexName)
             new SemanticConfiguration(semanticSearchConfig, new SemanticPrioritizedFields
             {
                 TitleField = new SemanticField("title"),
-                ContentFields =
-                {
-                    new SemanticField("content")
-                }
+                ContentFields = { new SemanticField("content") }
             })
         }
     },
     Fields =
     {
         new SimpleField("id", SearchFieldDataType.String)
-            { IsKey = true, IsFilterable = true, IsSortable = true, IsFacetable = true },
+        {
+            IsKey = true,
+            IsFilterable = true,
+            IsSortable = true,
+            IsFacetable = true
+        },
         new SearchableField("title") { IsFilterable = true },
         new SearchableField("content") { IsFilterable = true },
         new SearchField("contentVector", SearchFieldDataType.Collection(SearchFieldDataType.Single))
@@ -125,20 +133,31 @@ SearchIndex searchIndex = new(indexName)
 
 // Create the search index
 progress.Report("Starting index creation...");
+stopwatch.Restart();
 await indexClient.CreateOrUpdateIndexAsync(searchIndex);
-progress.Report("Index creation completed.");
+stopwatch.Stop();
+progress.Report($"Index creation completed. (Elapsed time: {stopwatch.ElapsedMilliseconds} ms)");
 
 // Initialize search client
 progress.Report("Initializing search client...");
+stopwatch.Restart();
 SearchClient searchClient = indexClient.GetSearchClient(indexName);
-progress.Report("Search client initialized.");
+stopwatch.Stop();
+progress.Report($"Search client initialized. (Elapsed time: {stopwatch.ElapsedMilliseconds} ms)");
+
+// Generate seed data using chat completion feature with Azure OpenAI
+List<ChatMessage> chatHistory =
+[
+    new SystemChatMessage("You are an HR manager at Contoso Hotels, expert at employee handbook creation."),
+    new UserChatMessage("Create a privacy policy, vacation policy, and describe a few job roles")
+];
 
 // Generate seed data using chat completion feature with Azure OpenAI
 progress.Report("Generating seed data using chat completion...");
-ClientResult<ChatCompletion>? seedData = await chatClient.CompleteChatAsync(
-    new SystemChatMessage("You are an HR manager at Contoso Hotels, expert at employee handbook creation."),
-    new UserChatMessage("Create a privacy policy, vacation policy, and describe a few job roles")
-);
+stopwatch.Restart();
+ClientResult<ChatCompletion>? seedData = await chatClient.CompleteChatAsync(chatHistory);
+stopwatch.Stop();
+progress.Report($"Seed data generation completed. (Elapsed time: {stopwatch.ElapsedMilliseconds} ms)");
 
 // Collect the seed data from chat completion text
 string text = string.Empty;
@@ -148,6 +167,7 @@ foreach (ChatMessageContentPart? message in seedData.Value.Content)
     Console.WriteLine($"{message.Kind}: {message.Text}");
     text += message.Text;
 }
+chatHistory.Add(new AssistantChatMessage(text));
 
 // Output the employee handbook to text file, first checking if it already exists
 string filePath = "employee_handbook.txt";
@@ -155,7 +175,6 @@ if (File.Exists(filePath))
 {
     File.Delete(filePath);
 }
-
 File.WriteAllText(filePath, text);
 
 // Open the text file for the user to review
@@ -164,7 +183,8 @@ progress.Report("Seed data generation completed.");
 
 // Chunk the text into smaller pieces
 progress.Report("Chunking text into smaller pieces...");
-const int maxChunkSize = 200;
+stopwatch.Restart();
+const int maxChunkSize = 1000;
 List<string> chunks = ChunkText(text, maxChunkSize);
 
 // Create documents from the chunks and upload them to the search index
@@ -179,37 +199,38 @@ foreach (string chunk in chunks)
         ContentVector = await GetEmbeddingAsync(chunk)
     });
 }
-
-progress.Report("Text chunking completed.");
+stopwatch.Stop();
+progress.Report($"Text chunking completed. (Elapsed time: {stopwatch.ElapsedMilliseconds} ms)");
 
 // Upload documents to the search index
 progress.Report("Uploading documents to the search index...");
+stopwatch.Restart();
 UploadDocuments(documents);
-progress.Report("Document upload completed.");
+stopwatch.Stop();
+progress.Report($"Document upload completed. (Elapsed time: {stopwatch.ElapsedMilliseconds} ms)");
 
 // Perform a search query on the index to test that it works
 progress.Report("Performing search query...");
+stopwatch.Restart();
 Response<SearchResults<RAGSearchDocument>>? ss = searchClient.Search<RAGSearchDocument>("employee");
 foreach (SearchResult<RAGSearchDocument> result in ss.Value.GetResults())
 {
     Console.WriteLine($"Id: {result.Document.Id}, Title: {result.Document.Title}, Content: {result.Document.Content}");
 }
-progress.Report("Search query completed.");
+stopwatch.Stop();
+progress.Report($"Search query completed. (Elapsed time: {stopwatch.ElapsedMilliseconds} ms)");
 
 // Generate good questions for employee to ask about the employee handbook
 progress.Report("Generating good questions for employee to ask...");
-seedData = await chatClient.CompleteChatAsync(
-    new SystemChatMessage("You are an HR manager at Contoso Hotels, expert on employee handbook."),
-    new UserChatMessage("Create a privacy policy, vacation policy, and describe a few job roles"),
-    new AssistantChatMessage(text),
-    new UserChatMessage("What are 3 good questions for an employee to ask about information that is in the employee handbook?")
-);
+stopwatch.Restart();
+chatHistory.Add(new UserChatMessage("What are 3 good questions for an employee to ask about information that is in the employee handbook you just wrote? Also what is the right answer that the employee should expect?"));
+seedData = await chatClient.CompleteChatAsync(chatHistory);
 Console.WriteLine("Ask these questions to the bot later:");
 text = string.Empty;
 foreach (ChatMessageContentPart? message in seedData.Value.Content)
 {
     // Output the data so we can see it and use it later
-    Console.WriteLine($"{message.Text}");
+    Console.WriteLine(message.Text);
     text += message.Text;
 }
 
@@ -219,13 +240,13 @@ if (File.Exists(filePath))
 {
     File.Delete(filePath);
 }
-
 File.WriteAllText(filePath, text);
 
 // Open the text file for the user to review
 Process.Start(new ProcessStartInfo(filePath) { UseShellExecute = true });
 
-progress.Report("Good questions generation completed.");
+stopwatch.Stop();
+progress.Report($"Good questions generation completed. (Elapsed time: {stopwatch.ElapsedMilliseconds} ms)");
 
 return;
 
