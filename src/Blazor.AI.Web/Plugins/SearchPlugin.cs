825d16b07e9e5378ee932ffd1c09b339bb1255da
Glen Souza <glenster75@gmail.com>
2025-06-16 22:14:03 -0700

    Update package references, enhance configuration, and improve code structure

--- src/Blazor.AI.Web/Plugins/SearchPlugin.cs
+++ src/Blazor.AI.Web/Plugins/SearchPlugin.cs
@@ -14,28 +14,28 @@ using Azure;
 using Azure.Search.Documents;
 using Azure.Search.Documents.Indexes;
 using Azure.Search.Documents.Models;
+using Microsoft.Extensions.AI;
 using Microsoft.SemanticKernel;
-using Microsoft.SemanticKernel.Embeddings;
 
 namespace Blazor.AI.Web.Plugins;
 
 #pragma warning disable SKEXP0001 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
 
 // ReSharper disable once ClassNeverInstantiated.Global
-public class SearchPlugin(ITextEmbeddingGenerationService textEmbeddingGenerationService, SearchIndexClient indexClient)
+public class SearchPlugin(IEmbeddingGenerator<string, Embedding<float>> embeddingGenerator, SearchIndexClient indexClient)
 {
     [KernelFunction("contoso_search")]
     [Description("Search documents for employer Contoso")]
     public async Task<string> SearchAsync([Description("The users optimized semantic search query")] string query)
     {
         // Convert string query to vector
-        ReadOnlyMemory<float> embedding = await textEmbeddingGenerationService.GenerateEmbeddingAsync(query);
+        Embedding<float> embedding = await embeddingGenerator.GenerateAsync(query);
 
         // Set the index to use in AI Search, the hard coded "demo" index was established in the SeedData project
         SearchClient searchClient = indexClient.GetSearchClient("demo");
 
         // Configure request parameters
-        VectorizedQuery vectorQuery = new(embedding);
+        VectorizedQuery vectorQuery = new(embedding.Vector);
         vectorQuery.Fields.Add("contentVector"); // name of the vector field from Azure AI Search schema established in the SeedData project 
 
         // Perform search request
